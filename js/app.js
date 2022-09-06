b301b6568c9464da1b3dc0c91e03abb12afd6ef8
Orlando Garcia <orlando.garcia.64@hotmail.com>
2022-09-06 15:12:03 -0500

    Simple form in website

--- js/app.js
+++ js/app.js
@@ -1,26 +1,4 @@
-console.log('Firebase SDK install and test running....')
-// // Required for side-effects
-// // /node_modules/
-// import firebase from '/node_modules/firebase/firebase';
-// import '/node_modules/firestore';
-// // Initialize Cloud Firestore and get a reference to the service
-// const db = firebase.firestore();
-
-// db.collection("uiJRKP1vvFDskL6x4rOg").add({
-//     company: "test-1",
-//     form_comments: "lorem-test",
-//     full_name: "Orlando Garcia",
-//     phone_number: "+528180913767",
-//     work_email: "oeg@cbord.com",
-// })
-// .then((docRef) => {
-//     console.log("Document written with ID: ", docRef.id);
-// })
-// .catch((error) => {
-//     console.error("Error adding document: ", error);
-// });
-
-
+console.log('Firebase SDK install and test running....');
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-app.js";
 import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -42,22 +20,32 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-console.log('db', db)
-
 // Add a second document with a generated ID.
 
-
-
-try {
-    const docRef = await addDoc(collection(db, "registro-contacto-web"), {
-        company: "test-1",
-        form_comments: "lorem-test",
-        full_name: "Orlando Garcia",
-        phone_number: "+528180913767",
-        work_email: "oeg@cbord.com",
-    });
-
-    console.log("Document written with ID: ", docRef.id);
-} catch (e) {
-    console.error("Error adding document: ", e);
+// Firestore add contact function
+const firestoreAddContact = async (formContactDTO) => {
+    try {
+        console.log('formContactDTO', formContactDTO);
+        const docRef = await addDoc(collection(db, "registro-contacto-web"), formContactDTO);
+        console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+        console.error("Error adding document: ", e);
+    }
 }
+
+// scripting logic
+
+const formContact = document.getElementById('formContact');
+
+formContact.addEventListener('submit', e => {
+    e.preventDefault();
+    const { full_name, work_email, company , phone_number, message} = e.currentTarget;
+    const formContactDTO = {
+        company: company.value,
+        form_comments: message.value,
+        full_name: full_name.value,
+        phone_number: phone_number.value,
+        work_email: work_email.value
+    }
+    firestoreAddContact(formContactDTO);
+});
